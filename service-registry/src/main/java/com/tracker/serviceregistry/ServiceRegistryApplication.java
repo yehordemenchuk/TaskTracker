83493c067bf1672f619f203a0362b621af69cbd2
Yehor Demenchuk <demenchuk1210m@gmail.com>
2025-07-04 14:41:00 +0200

    fix .properties file

--- service-registry/src/main/java/com/tracker/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/tracker/serviceregistry/ServiceRegistryApplication.java
@@ -1,4 +1,4 @@
-package com.tracker.service_registry;
+package com.tracker.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- service-registry/src/test/java/com/tracker/serviceregistry/ServiceRegistryApplicationTests.java
+++ service-registry/src/test/java/com/tracker/serviceregistry/ServiceRegistryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tracker.service_registry;
+package com.tracker.serviceregistry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
